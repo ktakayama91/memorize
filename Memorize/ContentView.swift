d752d62dc1e0b2413d1da29dae9f53355c11610d
Kei Takayama <ktakayama.91@gmail.com>
2021-08-19 20:12:54 -0500

    Add and remove cards from the view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,28 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚗", "🚙", "🚕", "🚓", "🚌", "🚎", "🏎", "🚑", "🚒", "🚐", "🚚", "🛻", "🚛", "🚜", "🛺", "🛴", "🚲", "🛵", "🏍", "🚃"]
+    @State var emojiCount = 4
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: false)
+        VStack{
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+
+    }
+    var add: some View {
+//      Option 1
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+    }
+    
+    var remove: some View {
+//      Option 2
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
     }
 }
 
 struct CardView: View {
-    @State var isFaceUp: Bool
+    var content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         return ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp {
-                shape.fill().foregroundColor(.red)
+                shape.fill().foregroundColor(.white)
                 shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
